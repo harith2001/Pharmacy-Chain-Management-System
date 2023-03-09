9fa0fbe1f7fe17acd644764a464d6dde9408bae4
harith vithanage <danula243@gmail.com>
2023-03-09 20:58:17 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 import Navbaar from './components/Navbaar';
+import Home from './components/Home';
+import Insert from './components/Insert';
 
 function App() {
   return (
     <>
     <Navbaar/>
+    <Home/>
+    <Insert/>
     </>
   );
 }
